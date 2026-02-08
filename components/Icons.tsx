05f5201a5bcae16a83cfc7e49eae8a3348545fdd
yashkanttiwary <yashkanttiwary@gmail.com>
2026-02-09 01:28:05 +0530

    refactor: Remove unused React import from Icons
    The `Icons` component no longer requires a React import as it's only exporting SVG components.

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export const Icons = {
   Camera: () => <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M14.5 4h-5L7 7H4a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-3l-2.5-3z"/><circle cx="12" cy="13" r="3"/></svg>,
   Pencil: () => <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"><path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"/></svg>,
